be60f0fffcf84bcb767d92fe2862a55923d32188
Brixjohn <brlumabi@gmail.com>
2018-07-19 14:28:39 +0800

    Added endpoint for sending message through FB's Send API

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import todo_hook from './todo'
+import todo_hook from './todo';
+import fetch from 'node-fetch';
+import { request } from 'https';
 
 export default ({ config, db }) => {
 	let routes = Router();
-
-	// add middleware here
 
 	let id = 0
 	let subject = ''
@@ -19,6 +19,7 @@ export default ({ config, db }) => {
 			priority = req.body.result.parameters.priority
 			todo_hook.todo_create(db, priority, subject, content, res)
 		} else if(req.body.result.action === "todo-view-all") {
+			console.log("USER ID: " + req.body.originalRequest.data.sender.id)
 			todo_hook.todo_view_all(db, res)
 		} else if(req.body.result.action === "todo-update") {
 			id = req.body.result.parameters.id
@@ -34,6 +35,31 @@ export default ({ config, db }) => {
 		} else {
 			console.log(req.body)
 		}
+	})
+
+	// SENDS MESSAGE THROUGH FACEBOOK MESSENGER'S SEND API
+	routes.post('/postStatus', (req, res) => {
+		fetch('https://graph.facebook.com/v3.0/me/messages?access_token=EAAC37iwg6CkBANy77fq5dtHqGwor27gSQda2wigVZAKCYXsbhzRfMA4sNnZCZCwXiY4nPzUlY3oLgmWTJQZAwfz78yz49eKZBXxMO3TcOxTnFmdNwZAK7ytWuNn19TKycaOaAeuFZAMOB95nZBZBnulVuMBHUY6GgnZAXa4DPmXzua1vGFZBkpOGrh0cdtQ5wClZCgYZD', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				"recipient": {
+					"id": req.query.id
+				},
+				"message": {
+					"text": "Hi! How about adding some tasks?"
+				}
+			})
+		})
+			.then(res => {
+				console.log("SUCCESSFULLY SENT A MESSAGE")
+				return res.json({})
+			}).catch(err => {
+				console.error(`Something went wrong`, err)
+				return res.json({})
+			})
 	})
 
 	// PRIVACY POLICY FOR FACEBOOK INTEGRATION
